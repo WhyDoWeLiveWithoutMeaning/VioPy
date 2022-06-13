232a81b34208226e699d6fa82832f11b247d30d7
Eric Beaulne <eric.minecraft20@gmail.com>
2022-06-13 11:51:15 -0400

    bruh honestly

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,6 @@ author = 'Meaning'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'builder',
     'sphinx.ext.autodoc',
     'sphinx.ext.extlinks',
     'sphinx.ext.intersphinx',
